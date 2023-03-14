2e742386ab4e8de123ee9f2a4a5287a2120c6f33
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-03-14 15:53:03 -0400

    saving work

--- Project 2/models/deck.js
+++ Project 2/models/deck.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
-    format: "Modern",
-    name: "Affinity",
-    archetype: "Aggro",
-    img: "https://mtgtop8.com/metas_thumbs/189.jpg",
+    format: "cEDH",
+    name: "Hinata's Golden Crown",
+    archetype: "Control/Combo",
+    img: "https://assets.moxfield.net/cards/card-436AO-art_crop.jpg",
     lands: [
-      "Glimmervoid x4", "Silverbluff Bridge x3", "Urza's Saga x4", "Darksteel Citadel x4", "Otawara, Soaring City x2", "Treasure Vault x2"],
+      ""],
     creatures: [
-      "Patchwork Automation x2", "Haywire Mite x1", "Sojourner's Companion x4", "Thought Monitor x 4", "Ornithopter x4", "Etched Champion x 2", "Memite x2", "Frogmite x3"],
-    planeswalkers: "",
-    artifacts: ["Cranial Plating x3", "Nettlecyst", "Veil of Assimilation", "Springleaf Drum", "Aether Spellbomb", "Relic of Progenitus", "Shadowspear"],
-    instants: ["Galvanic Blast x4"],
-    sorceries: ["Thoughtcast x2", "Forging the Anchor x4"],
+      "Hinata, Dawn-Crowned"],
+    planeswalkers: "Grist, the Hunger Tide",
+    artifacts: ["Umezawa's Jitte", "Kaldra Compleat"],
+    instants: "Swords to Plowshares x 4",
+    sorceries: ["Green Sun's Zenith x4", "Prismatic Ending x2"],
     enchantments: "",
   },
 ];
